755b0cf6709abf352c97054f77d834e323d7a7e9
Frances Jurek <>
2023-11-06 21:36:29 -0900

    complete exercise 5.1

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,20 @@ export const ToastContext = React.createContext();
 function ToastProvider({children}) {
   const [allToasts, setAllToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleEscapeToDismissToasts(event) {
+      if (event.code === 'Escape') {
+        setAllToasts([]);
+      }
+    }
+
+    window.addEventListener('keydown', handleEscapeToDismissToasts);
+
+    return () => {
+      window.removeEventListener('keydown', handleEscapeToDismissToasts);
+    }
+  }, []);
+
   function removeToast(toastId) {
     const updatedToasts = allToasts.filter(({ id }) => id !== toastId);
 
